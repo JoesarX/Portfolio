a9be2326e3e1d4d75250704fd9b70ca6cfa15738
Josue <josueespinalsanz@gmail.com>
2023-11-04 17:21:01 -0600

    lamaDev Contributions

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
-import "./app.scss"
+import Test from "./Test";
+import "./app.scss";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
 
 const App = () => {
-  return <div>
-    <section id="homepage">
-      <Navbar />
-      <Hero/>
-    </section>
-    <section>Hero</section>
-    <section id="services">Parallax</section>
-    <section>Services</section>
-    <section id="proyects">Parallax</section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section id="contact">Contact</section>
-
-  </div>;
+  return (
+    <div>
+      {/* <Cursor /> */}
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+      {/* Framer Motion Crash Course */}
+      {/* <Test/>
+    <Test/> */}
+    </div>
+  );
 };
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,37 @@
-import { motion } from 'framer-motion'
-import "./navbar.scss"
-import Sidebar from '../sidebar/Sidebar'
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       {/* Sidebar */}
-      <Sidebar />
-      <div className='wrapper'>
-        <motion.span initial={{ opacity: 0, scale: 0.5 }}
+      <Sidebar/>
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}>
+          transition={{ duration: 0.5 }}
+        >
           Josue Espinal
         </motion.span>
-        <div className='social'>
-          <a href='https://www.instagram.com/josue.espinal_/'><img src="/instagram.png" alt="" /></a>
-          <a href='https://www.instagram.com/josue.espinal_/'><img src="/instagram.png" alt="" /></a>
-          <a href='https://www.instagram.com/josue.espinal_/'><img src="/instagram.png" alt="" /></a>
-          <a href='https://www.instagram.com/josue.espinal_/'><img src="/instagram.png" alt="" /></a>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/dribbble.png" alt="" />
+          </a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
